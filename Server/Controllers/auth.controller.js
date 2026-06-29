2700022c536f3e6c0b65123ce04a4b02449a1902
Shuhel15 <shuhelahmed3789@gmail.com>
2026-06-29 20:13:02 +0530

    error fix

--- Server/Controllers/auth.controller.js
+++ Server/Controllers/auth.controller.js
@@ -16,7 +16,7 @@ export const googleAuth = async (req, res) => {
     }
     const token = await genToken(user._id);
     res.cookie("token", token, {
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
